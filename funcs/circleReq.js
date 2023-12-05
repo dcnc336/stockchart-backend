d601af7124010cebb6a3b8ac6dc216836b77c8a0
dcnc336 <dcnc336@gmail.com>
2023-12-05 18:52:45 -0500

    update axios part

--- funcs/circleReq.js
+++ funcs/circleReq.js
@@ -1,45 +1,35 @@
 const axios = require('axios');
-
-// const options = {
-//   method: 'GET',
-//   url: 'https://alpha-vantage.p.rapidapi.com/query',
-//   params: {
-//     interval: '60min',
-//     function: 'TIME_SERIES_INTRADAY',
-//     symbol: 'MSFT',
-//     datatype: 'json',
-//     output_size: 'compact'
-//   },
-//   headers: {
-//     'X-RapidAPI-Key': process.env.X_RapidAPI_Key,
-//     'X-RapidAPI-Host': process.env.X_RapidAPI_Host
-//   }
-// };
-
-// try {
-// 	const response = await axios.request(options);
-// 	console.log(response.data);
-// } catch (error) {
-// 	console.error(error);
-// }
-
-// function logMessage() {
-    //  console.log('Cron job executed at:', new Date().toLocaleString());
-    // }
-    // // Schedule the cron job to run every minute
-    // cron.schedule('* * * * *', () => {
-        //  logMessage();
-        // });
-
 const cron = require('node-cron');
-const getTimeSeriesIntraday = () => {
-    console.log('ajajajajajjajaja');
+const socketIO = require('./socketIO');
+
+const options = {
+    method: 'GET',
+    url: 'https://alpha-vantage.p.rapidapi.com/query',
+    params: {
+      function: 'TIME_SERIES_DAILY_ADJUSTED',
+      symbol: 'MSFT',
+      outputsize: 'compact',
+      datatype: 'json'
+    },
+    headers: {
+      'X-RapidAPI-Key': process.env.X_RapidAPI_Key,
+      'X-RapidAPI-Host': process.env.X_RapidAPI_Host
+    }
+};
+
+const getTimeSeriesIntraday = async() => {
+    try {
+        const response = await axios.request(options);
+        console.log(response.data);
+    } catch (error) {
+        console.error(error);
+    }
 }
 const circleReq = {
     init:()=>{
-        cron.schedule('* * * * *', () => {
+        // cron.schedule('* * * * *', () => {
             getTimeSeriesIntraday();
-        });
+        // });
     }
 }
 
